a7a39a5c12f870347bf8e8adcca0c3a01a42add0
cath3r0 <ilghvm@gmail.com>
2017-02-18 17:58:53 +0200

    Add possibility to choose what person wants to calculate

--- Payster.py
+++ Payster.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request
 
 
-# moneyAmount = 0
-# hours = 0
-# days = 0
-
 app = Flask(__name__)
 
 
@@ -13,16 +9,40 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/calculate', methods=['POST', 'GET'])
+@app.route('/calculate', methods=['POST'])
 def result():
     if request.method == 'POST':
-        money_amount = int(request.form['moneyAmount'])
-        hours = int(request.form['hours'])
-        days = int(request.form['days'])
-        calculated = round(money_amount / days / hours, 2)
-        return render_template("calculate.html", result=calculated)
-    elif request.method == 'GET':
-        return render_template("calculate.html", result='You are not allowed to go here')
+        if request.form['money_type'] == 'monthly':
+            money_type = "payment per hour"
+            calculated = calculate_per_hour(request.form)
+        elif request.form['money_type'] == 'hourly':
+            money_type = "month salary"
+            calculated = calculate_monthly(request.form)
+        else:
+            return render_template('index.html')
+        return render_template("calculate.html", result=(calculated, money_type))
+
+
+def calculate_per_hour(request_form):
+    money_amount = int(request_form['moneyAmount'])
+    hours = int(request_form['hours'])
+    days = int(request_form['days'])
+    calculated = round(money_amount / days / hours, 2)
+    return calculated
+
+
+def calculate_monthly(request_form):
+    money_amount = float(request_form['moneyAmount'])
+    hours = int(request_form['hours'])
+    days = int(request_form['days'])
+    calculated = round(money_amount * days * hours, 2)
+    return calculated
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.error('Page not found: %s', request.path)
+    return render_template('404.htm')
 
 
 if __name__ == '__main__':
