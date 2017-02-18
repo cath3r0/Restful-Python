7c319aae611c7527de873f9cddd8539e95416ace
cath3r0 <ilghvm@gmail.com>
2017-02-18 10:04:26 +0200

    Add logic before UI: * Now you  can calculate payment per hour if you know your month salary

--- Payster.py
+++ Payster.py
@@ -1,5 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+
+
+# moneyAmount = 0
+# hours = 0
+# days = 0
 
 app = Flask(__name__)
 
@@ -9,5 +13,17 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/calculate', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        money_amount = int(request.form['moneyAmount'])
+        hours = int(request.form['hours'])
+        days = int(request.form['days'])
+        calculated = round(money_amount / days / hours, 2)
+        return render_template("calculate.html", result=calculated)
+    elif request.method == 'GET':
+        return render_template("calculate.html", result='You are not allowed to go here')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
